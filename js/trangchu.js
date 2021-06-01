3dfecb614ecee7d27d98f2d7ca59796ee6e56498
nthien2502 <nthien390@gmail.com>
2021-06-02 01:09:07 +0700

    WEB207 - frontend framework

---
+++ js/trangchu.js
@@ -0,0 +1,10 @@
+// scroll nav active
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
